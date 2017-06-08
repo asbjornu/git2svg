7ea18cbd336d12e7c932c02a91899e7aae35318b
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2017-06-08 22:48:50 +0200

    Handle when no arguments are passed

--- src/args.js
+++ src/args.js
@@ -5,7 +5,7 @@ module.exports = {
               .usage('\nUsage: npm start -- [repository_path]')
               .epilog(`Asbjørn Ulsberg © ${new Date().getFullYear()}`)
               .check((arguments, y) => {
-                  if (!arguments._ || !Array.isArray(arguments._) || arguments._.length != 1) {
+                  if (!arguments._ || !Array.isArray(arguments._) || arguments._.length != 1 || arguments._[0] === `${arguments.$0}.js`) {
                       throw 'Error: "repository_path" argument missing or invalid';
                   }
 
