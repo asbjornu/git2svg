fd0ff4a2533e26ead3cf63a6a87e7f65376bf7b6
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2017-06-09 00:49:44 +0200

    Watch the .git folder and emit commit data to the browser

--- src/app.js
+++ src/app.js
@@ -29,15 +29,14 @@ module.exports.start = express => {
 				.then(repository => {
 					const http = require('http').Server(server);
 					const io = require('socket.io')(http);
+					const gitFolder = repository.path();
 
 					io.on('connection', socket => {
-  						console.log('a user connected');
+  						console.log('Browser connected through socket.io');
+						refresh('connected');
 					});
 
-					fs.watch(path, { recursive: true }, (event, filename) => {
-						console.log('Git repository changed.', event, filename);
-						io.emit('data', { event: event, filename: filename });
-					});
+					fs.watch(gitFolder, { recursive: true }, refresh);
 
 					server.locals.io = io;
 					server.locals.path = path;
@@ -45,7 +44,7 @@ module.exports.start = express => {
 
 					var port = process.env.PORT || 3000;
 					http.listen(port, () => {
-						console.log(`Watching '${path}' on http://localhost:${port}`);
+						console.log(`Watching <${gitFolder}> on <http://localhost:${port}/>`);
 					});
 				});
 		});
@@ -69,3 +68,50 @@ module.exports.showIndex = (request, response, next) => {
 		next(error);
     }
 };
+
+
+function refresh(event, filename) {
+	console.log(`${event}: Loading data from the Git repository.`);
+	var repository = server.locals.repository;
+
+	var getCommitData = commit => {
+		var sha = commit.sha();
+		var author = commit.author().toString();
+		var committer = commit.committer().toString();
+		var message = commit.message();
+		var body = commit.body();
+		var time = commit.timeMs();
+		return {
+			sha: sha,
+			author: author,
+			committer: committer,
+			message: message,
+			body: body,
+			parents: null,
+			time: time
+		};
+	};
+
+	Git.Reference.list(repository).then(references => {
+		for (var reference of references) {
+			Git.Reference.nameToId(repository, reference).then(oid => {
+				return Git.Commit.lookup(repository, oid);
+			}).then(commit => {
+				var commitData = getCommitData(commit);
+
+				Promise
+					.all(commit.parents().map(poid => Git.Commit.lookup(repository, poid)))
+					.then(parents => {
+						commitData.parents = parents.map(getCommitData);
+						server.locals.io.emit('data', {
+							commit: commitData
+						});
+					})
+			}).catch(error => {
+				console.error(error);
+			});
+		}
+	}).catch(error => {
+		console.error(error);
+	});
+}
